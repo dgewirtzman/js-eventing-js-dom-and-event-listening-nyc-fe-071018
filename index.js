93474cfe46d8123cac6ffe6b61e21aacab7bff9a
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-21 23:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const main = document.querySelector('#main')
+const paragraph = document.querySelector('p')
 
 main.addEventListener('click', (event) => {
   alert('Weeeeee! I was clicked!')
 })
 
+paragraph.addEventListener('click', (event) => {
+  alert('You clicked the p!')
+})
